c95fb39853957fa60e1cebe8526d721152911401
Farzan Pira <farzanpira3@gmail.com>
2026-07-20 23:04:58 +0530

    updated the tsconfig structure and added prisma config, schemas, and migrations.

---
+++ services/users-ms/prisma/migrations/20260720170350_updated_fields/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `lastUpdated` on the `User` table. All the data in the column will be lost.
+  - Added the required column `updatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "lastUpdated",
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL,
+ALTER COLUMN "createdAt" SET DEFAULT CURRENT_TIMESTAMP;
